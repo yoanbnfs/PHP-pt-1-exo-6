dd4b0456d23b3e34ad57022eb3d86da11453d6bc
Yoan Bonnefis <yoan.bonnefis@yahoo.fr>
2017-12-12 14:15:33 +0100

    exercice

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>exo-6</title>
+  </head>
+  <body>
+    <?php
+      $nom = 'Jean-Eude';
+      echo 'bonjour ' . $nom . ' , comment vas tu ?'
+    ?>
+  </body>
+</html>
